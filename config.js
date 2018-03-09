d898521b0e0a53bb2777cf4982a8ef46f70f8b2a
kmi cool <c_matasalinas@fanshaweonline.ca>
2018-03-08 20:02:45 -0600

    initial files

---
+++ config.js
@@ -0,0 +1,11 @@
+var config = {
+  user : "root",
+  password : "", //leave blank for windows
+  port: 8889,
+  host: "localhost",
+  database: "cooperstuff", // database
+
+  kidsmode : false
+}
+
+module.exports = config;
